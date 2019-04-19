f98d31bf2e9ade2b7489d3bbf6a84635ddf036d3
Quin Outland <qoutland@gmail.com>
2019-04-19 14:27:43 -0700

    first commit on Koutla01

---
+++ public/README.md
@@ -0,0 +1,15 @@
+# Champion Fleet Department Page
+
+## Change Instructions 
+(Changes should push in about 30 secs after commiting them)
+
+### Uploading a PDF:
+1. Navigate to *public/pdfs/* folder and select the PDF you want to change.
+2. Remove the old PDF (trashcan icon) and commit the changes.
+3. Upload a new file and commit the changes.
+
+### Changing Data in the tables:
+1. Navigate to *public/data.js*.
+2. Change any data, follow the standard data format.
+3. If you would like to check your formatting use this: [JSON Validator](https://jsonlint.com/).
+4. Commit the changes.
---
+++ public/data.js
@@ -0,0 +1,486 @@
+data = {
+    "Passenger Vehicles": [{
+            "Item #": "1.1",
+            "Type": "SEDAN",
+            "Size": "Full Size",
+            "Doors": "4",
+            "Passengers": "6",
+            "Final Order Date": "",
+            "Pdf": "chevy_1-1.pdf"
+        },
+        {
+            "Item #": "1.2",
+            "Type": "SEDAN",
+            "Size": "Intermediate",
+            "Doors": "4",
+            "Passengers": "5-6",
+            "Final Order Date": "3/18/2019",
+            "Pdf": "chevy_1-2.pdf"
+        },
+        {
+            "Item #": "1.3",
+            "Type": "SEDAN",
+            "Size": "Full Size",
+            "Doors": "4",
+            "Passengers": "6",
+            "Final Order Date": "11/29/2018",
+            "Pdf": "chevy_1-3.pdf"
+        }
+    ],
+    "Pickups": [{
+            "Item #": "2.2",
+            "Type": "TRUCK",
+            "Size": "Compact",
+            "Cab": "Extended",
+            "Bed": "Long",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-2.pdf"
+        },
+        {
+            "Item #": "2.3",
+            "Type": "TRUCK",
+            "Size": "Compact",
+            "Cab": "Crew",
+            "Bed": "Short",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-3.pdf"
+        },
+        {
+            "Item #": "2.4",
+            "Type": "TRUCK",
+            "Size": "Compact",
+            "Cab": "Crew",
+            "Bed": "Long",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-4.pdf"
+        },
+        {
+            "Item #": "2.5",
+            "Type": "TRUCK",
+            "Size": "1/2 Ton",
+            "Cab": "Standard",
+            "Bed": "Short",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-5.pdf"
+        },
+        {
+            "Item #": "2.6",
+            "Type": "TRUCK",
+            "Size": "1/2 Ton",
+            "Cab": "Standard",
+            "Bed": "Long",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-6.pdf"
+        },
+        {
+            "Item #": "2.7",
+            "Type": "TRUCK",
+            "Size": "1/2 Ton",
+            "Cab": "Crew",
+            "Bed": "Short",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-7.pdf"
+        },
+        {
+            "Item #": "2.8",
+            "Type": "TRUCK",
+            "Size": "1/2 Ton",
+            "Cab": "Crew",
+            "Bed": "Long",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-8.pdf"
+        },
+        {
+            "Item #": "2.9",
+            "Type": "TRUCK",
+            "Size": "1/2 Ton",
+            "Cab": "Extended",
+            "Bed": "Short",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-9.pdf"
+        },
+        {
+            "Item #": "2.9A",
+            "Type": "TRUCK",
+            "Size": "1/2 Ton",
+            "Cab": "Extended",
+            "Bed": "Short",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-9A.pdf"
+        },
+        {
+            "Item #": "2.12",
+            "Type": "TRUCK",
+            "Size": "3/4 Ton",
+            "Cab": "Standard",
+            "Bed": "Long",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-12.pdf"
+        },
+        {
+            "Item #": "2.13",
+            "Type": "TRUCK",
+            "Size": "3/4 Ton",
+            "Cab": "Crew",
+            "Bed": "Short",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-13.pdf"
+        },
+        {
+            "Item #": "2.14",
+            "Type": "TRUCK",
+            "Size": "3/4 Ton",
+            "Cab": "Crew",
+            "Bed": "Long",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-14.pdf"
+        },
+        {
+            "Item #": "2.15",
+            "Type": "TRUCK",
+            "Size": "3/4 Ton",
+            "Cab": "Extended",
+            "Bed": "Short",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-15.pdf"
+        },
+        {
+            "Item #": "2.16",
+            "Type": "TRUCK",
+            "Size": "3/4 Ton",
+            "Cab": "Extended",
+            "Bed": "Long",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-16.pdf"
+        },
+        {
+            "Item #": "2.18",
+            "Type": "TRUCK",
+            "Size": "1 Ton SRW",
+            "Cab": "Standard",
+            "Bed": "Long",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-18.pdf"
+        },
+        {
+            "Item #": "2.19",
+            "Type": "TRUCK",
+            "Size": "1 Ton SRW",
+            "Cab": "Crew",
+            "Bed": "Short",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-19.pdf"
+        },
+        {
+            "Item #": "2.20",
+            "Type": "TRUCK",
+            "Size": "1 Ton SRW",
+            "Cab": "Crew",
+            "Bed": "Long",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-20.pdf"
+        },
+        {
+            "Item #": "2.21",
+            "Type": "TRUCK",
+            "Size": "1 Ton SRW",
+            "Cab": "Extended",
+            "Bed": "Long",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-21.pdf"
+        },
+        {
+            "Item #": "2.22",
+            "Type": "TRUCK",
+            "Size": "1 Ton DRW",
+            "Cab": "Standard",
+            "Bed": "Long",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-22.pdf"
+        },
+        {
+            "Item #": "2.24",
+            "Type": "TRUCK",
+            "Size": "1 Ton DRW",
+            "Cab": "Crew",
+            "Bed": "Long",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-24.pdf"
+        },
+        {
+            "Item #": "2.26",
+            "Type": "TRUCK",
+            "Size": "1 Ton DRW",
+            "Cab": "Extended",
+            "Bed": "Long",
+            "Final Order Date": "4/25/2019",
+            "Pdf": "chevy_2-26.pdf"
+        }
+    ],
+    "Cab and Chassis": [{
+            "Item #": "3.1",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "3/4 Ton SRW",
+            "Cab": "Standard",
+            "GVW": "Approx. 8800lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-1.pdf"
+        },
+        {
+            "Item #": "3.2",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "3/4 Ton SRW",
+            "Cab": "Crew",
+            "GVW": "Approx. 8800lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-2.pdf"
+        },
+        {
+            "Item #": "3.3",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "3/4 Ton SRW",
+            "Cab": "Extended",
+            "GVW": "Approx. 8800lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-3.pdf"
+        },
+        {
+            "Item #": "3.4",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "1 Ton SRW",
+            "Cab": "Standard",
+            "GVW": "Approx. 11,000lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-4.pdf"
+        },
+        {
+            "Item #": "3.5",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "1 Ton SRW",
+            "Cab": "Crew",
+            "GVW": "Approx. 11,000lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-5.pdf"
+        },
+        {
+            "Item #": "3.6",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "1 Ton SRW",
+            "Cab": "Extended",
+            "GVW": "Approx. 11,000lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-6.pdf"
+        },
+        {
+            "Item #": "3.7",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "1 Ton DRW",
+            "Cab": "Standard",
+            "GVW": "Approx. 13,000lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-7.pdf"
+        },
+        {
+            "Item #": "3.8",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "1 Ton DRW",
+            "Cab": "Crew",
+            "GVW": "Approx. 11,000lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-8.pdf"
+        },
+        {
+            "Item #": "3.9",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "1 Ton DRW",
+            "Cab": "Extended",
+            "GVW": "Approx. 13,000lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-9.pdf"
+        },
+        {
+            "Item #": "3.10",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "Greater than 1 Ton",
+            "Cab": "Standard",
+            "GVW": "Approx. 16,500lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-10.pdf"
+        },
+        {
+            "Item #": "3.10A",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "Greater than 1 Ton",
+            "Cab": "Standard",
+            "GVW": "Approx. 17,500lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-10A.pdf"
+        },
+        {
+            "Item #": "3.10B",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "Greater than 1 Ton",
+            "Cab": "Regular",
+            "GVW": "Approx. 19,500lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-10B.pdf"
+        },
+        {
+            "Item #": "3.10C",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "Greater than 1 Ton",
+            "Cab": "Standard",
+            "GVW": "Approx. 21,500lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-10C.pdf"
+        },
+        {
+            "Item #": "3.11",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "Greater than 1 Ton",
+            "Cab": "Crew",
+            "GVW": "Approx. 16,500lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-11.pdf"
+        },
+        {
+            "Item #": "3.11A",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "Greater than 1 Ton",
+            "Cab": "Crew",
+            "GVW": "Approx. 17,500lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-11A.pdf"
+        },
+        {
+            "Item #": "3.11B",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "Greater than 1 Ton",
+            "Cab": "Crew",
+            "GVW": "Approx. 19,500lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-11B.pdf"
+        },
+        {
+            "Item #": "3.11C",
+            "Type": "CAB AND CHASSIS",
+            "Towing": "Greater than 1 Ton",
+            "Cab": "Crew",
+            "GVW": "Approx. 21,500lb",
+            "Final Order Date": "2/22/2018",
+            "Pdf": "chevy_3-11C.pdf"
+        }
+    ],
+    "Upfit Information": [{
+            "Distributor": "Sierra Truck Body and Equipment",
+            "Pdf": "sierra_truck_body_and_equipment.pdf"
+        },
+        {
+            "Distributor": "Harbor Truck and Van",
+            "Pdf": "harbor_truck_and_van.pdf"
+        },
+        {
+            "Distributor": "Diamond Truck Body Mfg - Animal Control Body Pricing 17422",
+            "Pdf": "diamond_truck_body_animal_control_body.pdf"
+        }
+    ],
+    "Passenger Vans": [{
+        "Item #": "4.1",
+        "Type": "PASSENGER VAN",
+        "Size/Towing": "Mini",
+        "Passengers": "8",
+        "Final Order Date": "3/21/2019",
+        "Pdf": "chevy_4-1.pdf"
+    },
+    {
+        "Item #": "4.2",
+        "Type": "PASSENGER VAN",
+        "Size/Towing": "",
+        "Passengers": "12",
+        "Final Order Date": "3/21/2019",
+        "Pdf": "chevy_4-2.pdf"
+    },
+    {
+        "Item #": "4.4",
+        "Type": "PASSENGER VAN",
+        "Size/Towing": "1 Ton",
+        "Passengers": "15",
+        "Final Order Date": "3/21/2019",
+        "Pdf": "chevy_4-4.pdf"
+    }
+],
+    "Cargo Vans": [
+    {
+        "Item #": "4.5",
+        "Type": "CARGO VAN",
+        "Towing": "1/2 Ton",
+        "Final Order Date": "3/21/2019",
+        "Pdf": "chevy_4-5.pdf"
+    },
+    {
+        "Item #": "4.6",
+        "Type": "CARGO VAN",
+        "Towing": "3/4 Ton",
+        "Final Order Date": "3/21/2019",
+        "Pdf": "chevy_4-6.pdf"
+    },
+    {
+        "Item #": "4.7",
+        "Type": "CARGO VAN",
+        "Towing": "1 Ton",
+        "Final Order Date": "3/21/2019",
+        "Pdf": "chevy_4-7.pdf"
+    }
+ ],
+    "Sport Utility Vehicles": [{
+        "Item #": "5.1",
+        "Type": "SPORT UTILITY VEHICLE",
+        "Drivetrain": "4x4",
+        "Towing": "",
+        "Doors": "4",
+        "Passengers": "4-5",
+        "Final Order Date": "3/21/2019",
+        "Pdf": "chevy_5-1.pdf"
+    },
+    {
+        "Item #": "5.2",
+        "Type": "SPORT UTILITY VEHICLE",
+        "Drivetrain": "4x4",
+        "Towing": "",
+        "Doors": "4",
+        "Passengers": "7-8",
+        "Final Order Date": "3/21/2019",
+        "Pdf": "chevy_5-2.pdf"
+    },
+    {
+        "Item #": "5.3",
+        "Type": "SPORT UTILITY VEHICLE",
+        "Drivetrain": "4x4",
+        "Towing": "1/2 Ton",
+        "Doors": "6",
+        "Passengers": "5-6",
+        "Final Order Date": "3/21/2019",
+        "Pdf": "chevy_5-3.pdf"
+    },
+    {
+        "Item #": "5.5",
+        "Type": "SPORT UTILITY VEHICLE",
+        "Drivetrain": "4x4",
+        "Towing": "1/2 Ton",
+        "Doors": "6",
+        "Passengers": "8-9",
+        "Final Order Date": "3/21/2019",
+        "Pdf": "chevy_5-5.pdf"
+    },
+    {
+        "Item #": "5.6",
+        "Type": "SPORT UTILITY VEHICLE",
+        "Drivetrain": "4x4",
+        "Towing": "1 Ton",
+        "Doors": "6",
+        "Passengers": "8-9",
+        "Final Order Date": "3/21/2019",
+        "Pdf": "chevy_5-6.pdf"
+    }
+ ]
+}
