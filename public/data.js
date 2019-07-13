0adfaac57172c8dd81da7ea6b5fdbed9cbf1af8c
klemus775 <52015087+klemus775@users.noreply.github.com>
2019-07-13 08:34:42 -0700

    Update data.js

--- public/data.js
+++ public/data.js
@@ -5,6 +5,7 @@ data = {
 			"Size": "Full Size",
 			"Doors": "4",
 			"Passengers": "6",
+			"Final Order Date":,
 			"Pdf": "chevy_1-1.pdf"
 		},
 		{
@@ -13,6 +14,7 @@ data = {
 			"Size": "Intermediate",
 			"Doors": "4",
 			"Passengers": "5-6",
+			"Final Order Date":,
 			"Pdf": "chevy_1-2.pdf"
 		},
 		{
