20f22464bd032fa26126672661139dbfa6eb6219
klemus775 <52015087+klemus775@users.noreply.github.com>
2019-07-30 13:36:51 -0700

    Update data.js

--- public/data.js
+++ public/data.js
@@ -295,8 +295,8 @@ data = {
 			"Type": "CAB AND CHASSIS",
 			"Model": "1 Ton DRW",
 			"Cab": "Standard",
-			"GVW": "Approx. 13,000lb",
-			"Final Order Date": "2/22/2018",
+			"GVW": "Approx. 14,000lb",
+			"Final Order Date": "",
 			"Pdf": "chevy_3-7.pdf"
 		},
 		{
