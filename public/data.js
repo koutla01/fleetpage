72eb14f1235132accf108bdcc41727a371bd0e6a
klemus775 <52015087+klemus775@users.noreply.github.com>
2021-07-12 08:13:37 -0700

    Update data.js

--- public/data.js
+++ public/data.js
@@ -70,7 +70,7 @@ data = {
 			"Size": "Compact",
 			"Cab": "Crew",
 			"Bed": "Short",
-			"Final Order Date": "12/17/2020",
+			"Final Order Date": "1 AVAILABLE",
 			"Pdf": "chevy_2-3.pdf"
 		},
 		{
