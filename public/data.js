8da3a1d6e2010fe8422b3cdcb8373cf161267852
klemus775 <52015087+klemus775@users.noreply.github.com>
2021-12-20 15:53:53 -0800

    Update data.js

--- public/data.js
+++ public/data.js
@@ -522,7 +522,7 @@ data = {
     		"Drivetrain": "4x4",
     		"Doors": "4",
     		"Passengers": "5-6",
-    		"Final Order Date": "",
+    		"Final Order Date": "08/30/2021",
     		"Pdf": "chevy_5-3.pdf"
     	},
 	{
@@ -540,7 +540,7 @@ data = {
     		"Drivetrain": "4x4",
     		"Doors": "4",
     		"Passengers": "8-9",
-    		"Final Order Date": "",
+    		"Final Order Date": "08/30/2021",
     		"Pdf": "chevy_5-5.pdf"
     	},
     ],
