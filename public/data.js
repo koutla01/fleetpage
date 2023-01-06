2e1f519f1fb1e019b4f3c2a4bf38b29c7ca4d7d5
klemus775 <52015087+klemus775@users.noreply.github.com>
2023-01-06 10:38:48 -0800

    Update data.js

--- public/data.js
+++ public/data.js
@@ -70,7 +70,7 @@ data = {
 			"Size": "Compact",
 			"Cab": "Crew",
 			"Bed": "Short",
-			"Final Order Date": "Limited Availability",
+			"Final Order Date": "01/03/2023",
 			"Pdf": "chevy_2-3.pdf"
 		},
 		{
@@ -97,7 +97,7 @@ data = {
 			"Size": "1/2 Ton",
 			"Cab": "Standard",
 			"Bed": "Long",
-			"Final Order Date": "09/02/2021",
+			"Final Order Date": "05/26/2022",
 			"Pdf": "chevy_2-6.pdf"
 		},
 		{
@@ -106,7 +106,7 @@ data = {
 			"Size": "1/2 Ton",
 			"Cab": "Crew",
 			"Bed": "Short",
-			"Final Order Date": "09/12/2021",
+			"Final Order Date": "05/26/2022",
 			"Pdf": "chevy_2-7.pdf"
 		},
 		{
@@ -115,7 +115,7 @@ data = {
 			"Size": "1/2 Ton",
 			"Cab": "Crew",
 			"Bed": "Long",
-			"Final Order Date": "09/12/2021",
+			"Final Order Date": "05/26/2022",
 			"Pdf": "chevy_2-8.pdf"
 		},
 		{
@@ -124,7 +124,7 @@ data = {
 			"Size": "1/2 Ton",
 			"Cab": "Extended",
 			"Bed": "Short",
-			"Final Order Date": "09/12/2021",
+			"Final Order Date": "05/26/2022",
 			"Pdf": "chevy_2-9.pdf"
 		},
 		{
