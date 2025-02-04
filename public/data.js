e34526702a9da0e8e1801eb823cc9f3cb3135632
klemus775 <52015087+klemus775@users.noreply.github.com>
2025-02-04 07:38:03 -0800

    colo cutoff

--- public/data.js
+++ public/data.js
@@ -70,7 +70,7 @@ data = {
 			"Size": "Compact",
 			"Cab": "Crew",
 			"Bed": "Short",
-			"Final Order Date": "Limited Availability",
+			"Final Order Date": "02/28/2025",
 			"Pdf": "chevy_2-3.pdf"
 		},
 		{
