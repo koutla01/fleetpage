fd39b7f28a0554958190bfddb9263463e4460e5e
klemus775 <52015087+klemus775@users.noreply.github.com>
2025-07-09 14:25:05 -0700

    Update data.js

--- public/data.js
+++ public/data.js
@@ -133,7 +133,7 @@ data = {
 			"Size": "3/4 Ton",
 			"Cab": "Standard",
 			"Bed": "Long",
-			"Final Order Date": "05/27/2025",
+			"Final Order Date": "",
 			"Pdf": "chevy_2-12.pdf"
 		},
 		{
@@ -142,7 +142,7 @@ data = {
 			"Size": "3/4 Ton",
 			"Cab": "Crew",
 			"Bed": "Short",
-			"Final Order Date": "05/27/2025",
+			"Final Order Date": "",
 			"Pdf": "chevy_2-13.pdf"
 		},
 		{
